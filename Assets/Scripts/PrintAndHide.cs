92f1316cb73346bc00d5539b1fb5cc313c4275a3
Sezylrin <alastairlu2002@gmail.com>
2021-08-30 00:43:53 +1000

    hide script

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,15 +7,29 @@ public class PrintAndHide : MonoBehaviour
     public int i;
     public GameObject objName;
     public Renderer rend;
+    public int x;
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        x = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(objName.name + ":" + (++i));
+       
+            Debug.Log(objName.name + ":" + (++i));
+        
+        if (i == 100 && objName.tag=="Red")
+        {
+            objName.SetActive(false);
+        }
+        if (i == x && objName.tag == "Blue" )
+        {
+            rend.enabled = false;
+            
+        }
     }
-}
+        
+    }
